446ea2ea279c9678eeb2ebccbc00ea69838de0eb
zivwhd <zivwhd@gmail.com>
2024-09-12 11:41:42 +0300

    ii

--- src/vit_adaptors.py
+++ src/vit_adaptors.py
@@ -2,13 +2,6 @@
 from baselines.AttrViT.ViT_LRP import vit_base_patch16_224 as vit_LRP
 from baselines.AttrViT.ViT_explanation_generator import LRP
 
-model = vit_LRP(pretrained=True).cuda()
-model.eval()
-attribution_generator = LRP(model)
-transformer_attribution = attribution_generator.generate_LRP(inp, method="transformer_attribution", index=topidx).detach()
-transformer_attribution = transformer_attribution.reshape(1, 1, 14, 14)
-transformer_attribution = torch.nn.functional.interpolate(transformer_attribution, scale_factor=16, mode='bilinear')
-transformer_attribution = transformer_attribution.reshape(224, 224).data.cpu() ##.numpy()
 
 class AttrVitSaliencyCreator:
     def __init__(self):
