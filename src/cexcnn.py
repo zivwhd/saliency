e2ec117dd13629d8c560e19c38249eff38c1fc22
zivwhd <zivwhd@gmail.com>
2024-09-26 19:39:51 +0300

    ii

--- src/cexcnn.py
+++ src/cexcnn.py
@@ -1,17 +1,20 @@
 import torch
 import cv2
 import numpy as np
+import logging
 
 class CexCnnSaliencyCreator:
-    def __init__(self):
-        pass
+    def __init__(self, quanttiles=[0.25,0.5,0.75,0.9, 0.95]):
+        self.quantiles = quanttiles
 
     def __call__(self, me, inp, catidx):
         llayer = me.get_cex_conv_layer()
 
+        resp = {}
         orig_probs = torch.softmax(me.model(inp)[0], dim=0)
         delta_prob_list = []
         with torch.no_grad():
+            logging.debug("CexCnn: iterating over filters")
             for idx in range(llayer.out_channels):
                 keep = llayer.weight.data[idx].clone()
                 llayer.weight.data[idx] = 0.0
@@ -22,10 +25,11 @@ class CexCnnSaliencyCreator:
                 llayer.weight.data[idx] = keep
                 keep = None
                 torch.cuda.ipc_collect()
-
+            logging.debug("done with filters")
             delta_prob = torch.stack(delta_prob_list)
             threshold = 0             
-            resp = ((delta_prob > threshold) & (delta_prob > delta_prob.quantile(0.5)))
+            for qntl in self.quantiles:
+                resp[qntl] = ((delta_prob > threshold) & (delta_prob > delta_prob.quantile(qntl)))
 
         pooled_grads = None
         activations = None
@@ -51,6 +55,9 @@ class CexCnnSaliencyCreator:
         me.model.zero_grad()
         loss.backward()
 
-        heatmap = (activations.cpu().squeeze(0) * resp.unsqueeze(1).unsqueeze(1)).mean(dim=0).detach().clone()
-        sal = torch.tensor(cv2.resize(heatmap.numpy(), (224,224))).unsqueeze(0)
-        return {"CexCnn" : sal}
+        sal_dict = {}
+        for qntl, qresp in resp.items():
+            heatmap = (activations.cpu().squeeze(0) * qresp.unsqueeze(1).unsqueeze(1)).mean(dim=0).detach().clone()
+            sal = torch.tensor(cv2.resize(heatmap.numpy(), (224,224))).unsqueeze(0)
+            sal_dict[f"CexCnn_{qntl}"] = sal
+        return sal_dict
