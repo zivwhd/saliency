b3bfff51671ab5316e4aaa32c70f6fe635ad445d
zivwhd <zivwhd@gmail.com>
2024-09-29 10:07:29 +0300

    ii

--- src/bundle.py
+++ src/bundle.py
@@ -59,7 +59,7 @@ def shell(cmd):
     
 os.makedirs(DEST_PATH, exist_ok=True)    
 for idx, image_name in enumerate(images):    
-    print("### {idx} {image_name} ")
+    print(f"========== {idx} {image_name} ===========")
     shell(["cp", os.path.join(IMG_PATH, f"{image_name}.JPEG"), DEST_PATH])
 
     ptrn = os.path.join(BASE_PATH, f"saliency.run/results/*/saliency/*/{image_name}")
