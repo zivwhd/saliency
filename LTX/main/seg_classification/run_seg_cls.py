8ab3104288b422baa5db4b5b64154c6f239f2027
zivwhd <zivwhd@gmail.com>
2024-10-18 18:01:56 +0300

    ii

--- LTX/main/seg_classification/run_seg_cls.py
+++ LTX/main/seg_classification/run_seg_cls.py
@@ -251,7 +251,7 @@ if __name__ == '__main__':
         accelerator='gpu',
         #auto_select_gpus=True,        
         max_epochs=args.n_epochs,
-        gpus=1,
+        devices=1,
         progress_bar_refresh_rate=30,
         num_sanity_val_steps=0,
         default_root_dir=checkpoints_default_root_dir,
