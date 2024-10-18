9f4875ec4aba0c8ffbddb8c5a7e0a8f9c3f9fdb9
zivwhd <zivwhd@gmail.com>
2024-10-18 17:04:48 +0300

    ii

---
+++ batch/ltxlearn.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##resource allocation
+#SBATCH --ntasks=3
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16g
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=SAL1000
+#SBATCH --error=/home/weziv5/work/logs/%x-%j-%4t.err
+#SBATCH --output=/home/weziv5/work/logs/%x-%j-%4t.out
+
+hostname
+pwd
+
+## Modules
+
+module load "anaconda3/5.3.0"
+module load "CUDA/11.8.0"
+
+conda init bash
+source activate salsc
+
+echo "Current Env:" $CONDA_DEFAULT_ENV
+python -c "import torch; print('PyTorch version:', torch.__version__)"
+python -c "import torch; print('CUDA available:', torch.cuda.is_available())"
+
+srun python LTX/main/seg_classification/run_seg_cls.py
+echo "DONE"
