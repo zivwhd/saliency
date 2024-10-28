a5e177f924d40f310d5ccc3dd19984abf28ddc7f
zivwhd <zivwhd@gmail.com>
2024-10-28 17:23:14 +0200

    update

--- src/dispatch.py
+++ src/dispatch.py
@@ -33,8 +33,8 @@ def get_mcomp_sal_creator():
         segsize=48, nmasks=500, 
         baselines = baselines,
         groups=[        
-            dict(c_mask_completeness=1.0, c_completeness=0.1, c_tv=0.025, c_model=0),                
-            dict(c_mask_completeness=1.0, c_completeness=0.1, c_tv=0.025, c_model=0.05),        
+            dict(c_mask_completeness=1.0, c_completeness=0.1, c_tv=0.05, c_model=0),                
+            dict(c_mask_completeness=1.0, c_completeness=0.1, c_tv=0.05, c_model=0.05),        
     ])
     
 def get_emask_sal_creator():
