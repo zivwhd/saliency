651736a55dd0f040ee9ae4be92857d067d1c4897
zivwhd <zivwhd@gmail.com>
2024-11-03 10:02:37 +0200

    update

--- src/dispatch.py
+++ src/dispatch.py
@@ -56,7 +56,7 @@ def get_mcompvit_sal_creator():
     baselines = [ZeroBaseline()]
     return MultiCompExpCreator(
         desc="MComp",
-        segsize=[32], nmasks=500, 
+        segsize=[32], nmasks=1000, 
         baselines = baselines,
         groups=[
             dict(c_mask_completeness=1.0, c_completeness=0, c_tv=0.1, c_model=0, c_norm=True, c_activation="sigmoid"), 
