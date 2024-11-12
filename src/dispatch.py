1a9262f096de4509724dd8586e4effca74de6597
zivwhd <zivwhd@gmail.com>
2024-11-12 13:27:03 +0200

    update

--- src/dispatch.py
+++ src/dispatch.py
@@ -103,7 +103,7 @@ def get_abl_sal_creator(segsize=40, nmasks=500):
 
     baselines = [ZeroBaseline()]
 
-    basic = dict(c_mask_completeness=1.0, c_magnitude=0.05, c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False, 
+    basic = dict(c_mask_completeness=1.0, c_magnitude=0.01, c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False, 
                 c_activation="",  epochs=300, select_from=150)
 
     def modify(**kwargs):
