7b6399b81d1557b9e354963ca63dc9b3497995ee
zivwhd <zivwhd@gmail.com>
2024-11-13 03:05:12 +0200

    update

---
+++ src/reports.py
@@ -0,0 +1,7 @@
+
+import time, socket
+
+HOSTNAME = socket.gethostname()
+def report_duration(start_time, model_name, method, args):
+    duration = time.time() - start_time
+    print(f"DURATION,{HOSTNAME},{model_name},{method},{args},{duration}")
