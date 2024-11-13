673dce5ded4d146d972a372b1bf1888b1f39c051
zivwhd <zivwhd@gmail.com>
2024-11-13 03:22:49 +0200

    update

--- src/dispatch.py
+++ src/dispatch.py
@@ -88,8 +88,8 @@ def get_obench_sal_creator():
         IGSaliencyCreator(500),
         MultiCompExpCreator(desc="MComp", segsize=[40], nmasks=[500], 
                             groups=[dict(c_mask_completeness=1.0, c_completeness=0, c_tv=0.3, c_model=0.0, c_norm=False, c_activation="", epochs=300)]),
-        MultiCompExpCreator(desc="MComp", segsize=[40], nmasks=[500], 
-                            groups=[dict(c_mask_completeness=1.0, c_completeness=0, c_tv=0.3, c_model=0.0, c_norm=False, c_activation="", epochs=1)]),
+        #MultiCompExpCreator(desc="MComp", segsize=[40], nmasks=[500], 
+        #                    groups=[dict(c_mask_completeness=1.0, c_completeness=0, c_tv=0.3, c_model=0.0, c_norm=False, c_activation="", epochs=1)]),
         RiseSaliencyCreator(),
         CamSaliencyCreator(),
         DixCnnSaliencyCreator()
