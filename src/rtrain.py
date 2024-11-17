cb9058e7188e41c84e03af6aec3192b78d615143
zivwhd <zivwhd@gmail.com>
2024-11-17 10:31:51 +0200

    'bla'

--- src/rtrain.py
+++ src/rtrain.py
@@ -16,7 +16,7 @@ from dataset import ImagenetSource
 # Parameters
 data_dir = "/home/weziv5/work/data/imagenet/validation"
 batch_size = 64
-num_epochs = 10
+num_epochs = 1000
 num_classes = 1000
 
 
@@ -24,7 +24,7 @@ if not os.path.exists('models'):
     os.makedirs('models')
 
 def get_output_weights_path(idx):    
-    return f"models/densenet201_retrained_{idx}.pth"
+    return f"models/densenet201_retrainedB_{idx}.pth"
 
 # Device configuration
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
