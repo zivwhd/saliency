a517ca675d9a714ce4e897d057d1a2bc05c21b0c
zivwhd <zivwhd@gmail.com>
2024-11-18 03:10:21 +0200

    'bla'

--- src/sanity.py
+++ src/sanity.py
@@ -14,7 +14,11 @@ def randomize_layer(me, layer_index):
             continue
         modified = True
         logging.info(f"randomizing layer weights {name}")
-        random_weights = torch.randn_like(param.data) * param.data.std() * 4 + param.data.mean()        
+        if 'bias' in name:
+            random_weights = torch.randn_like(param.data) * 3 + 0.0
+        else:
+            random_weights = torch.randn_like(param.data) * 3 + 1.0
+        #random_weights = torch.randn_like(param.data) * param.data.std() * 4 + param.data.mean()        
         # Replace the weights
         param.data = random_weights
     assert modified
