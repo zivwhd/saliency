f81002bc58f383c595f984eca320696131ba2ee6
zivwhd <zivwhd@gmail.com>
2024-11-18 17:56:49 +0200

    bla

--- src/sanity.py
+++ src/sanity.py
@@ -9,28 +9,17 @@ from timm import create_model
 
 def randomize_layer(me, layer_index):    
     modified = False
-    for name, param in me.model.named_parameters():
-        if (f".denselayer{layer_index}." not in name):
-            continue
-        modified = True
-        logging.info(f"randomizing layer weights {name}")
-        if 'bias' in name:
-            #param.data = torch.randn_like(param.data)*0  + 1.0
-            logging.info("is bias")
-        elif 'conv' in name:
-            param.data = torch.randn_like(param.data) + 1.0
-            logging.info("is conv")
-        else:
-            logging.info("skipping")
-            
-        #random_weights = torch.randn_like(param.data) * param.data.std()  + param.data.mean()        
-        # Replace the weights
-        
-    assert modified
+    conv_layers = [x for x in me.model.named_parameters() if "conv" in x[0]]
+    param = conv_layers[layer_index][1]
+    param.data = torch.randn_like(param.data) 
 
 class SanityCreator:
     def __init__(self, nmasks=500, c_magnitude=0.01):
         self.lsc = CompExpCreator(nmasks=nmasks, segsize=40, c_mask_completeness=1.0, c_magnitude=c_magnitude, 
+                                  c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False, 
+                                 c_activation="",  epochs=300, select_from=None)
+        
+        self.lsca = CompExpCreator(nmasks=500, segsize=40, c_mask_completeness=1.0, c_magnitude=0.01, 
                                   c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False, 
                                  c_activation="",  epochs=300, select_from=None)
 
@@ -39,18 +28,21 @@ class SanityCreator:
         orig_model = me.model
         res = {}
         
+        conv_layers = [x for x in me.model.named_parameters() if "conv" in x[0]]
+        nlayers = len(conv_layers)
+        
         res["Base"] = self.lsc.explain(me, inp, catidx).cpu()
 
-        for idx in range (1, 33):
-            layer_id = 33 - idx
+        for idx in range(nlayers):
+            layer_id = nlayers - idx
             try:
                 me.model = copy.deepcopy(orig_model)
 
                 randomize_layer(me, idx)                
-                res[f"Rnd_{layer_id}"] = self.lsc.explain(me, inp, catidx).cpu()
-                for lidx in range(idx, 33):
+                res[f"Rnd_{layer_id}"] = self.lsca.explain(me, inp, catidx).cpu()
+                for lidx in range(idx, nlayers):
                     randomize_layer(me, lidx)
-                res[f"Csc_{layer_id}"] = self.lsc.explain(me, inp, catidx).cpu()
+                res[f"Csc_{layer_id}"] = self.lsca.explain(me, inp, catidx).cpu()
             finally:
                 me.model = orig_model
 
