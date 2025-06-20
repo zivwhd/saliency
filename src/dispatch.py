90458593eb94e62589e11325205bf03b527464ba
zivwhd <zivwhd@gmail.com>
2025-06-21 00:33:50 +0300

    next

--- src/dispatch.py
+++ src/dispatch.py
@@ -93,8 +93,9 @@ def get_mrcomp_sal_creator():
 
 def get_autocomp_sal_creator():
     # desc="AutoCompA",  NOT GOOD segsize=32, nmasks=1000, c_opt="Adam", lr=0.1, lr_step=9, lr_step_decay=0.9, epochs=501, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  
+    # desc="AutoCompB", segsize=[32], nmasks=[1000], c_opt="Adam", lr=0.1, lr_step=9, lr_step_decay=0.9,  epochs=301, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  c_activation="",
     return AutoCompExpCreator(
-        desc="AutoCompB", segsize=[32], nmasks=[1000], c_opt="Adam", lr=0.1, lr_step=9, lr_step_decay=0.9, 
+        desc="AutoCompC", segsize=[16,32,48], nmasks=[300,400,300], c_opt="Adam", lr=0.1, lr_step=9, lr_step_decay=0.9, 
         epochs=301, select_from=None, select_freq=3, select_del=1.0,
         c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False, 
         c_activation="",
