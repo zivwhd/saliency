3e668ce8f55e267e393b75be0199cf08f90d84c9
zivwhd <zivwhd@gmail.com>
2025-06-21 16:27:36 +0300

    next

--- src/benchmark.py
+++ src/benchmark.py
@@ -419,7 +419,7 @@ def bootstrap_ci(series, B=1000, alpha_low=5, alpha_high=95):
 
 def summarize_scores_df(df, extended=False, equant=False):
     meta_cols = ["model","image","variant"]
-    confidence_cols = ["pred_ins","pred_del"]
+    confidence_cols = ["pred_ins_auc","pred_del_auc"]
     metric_cols = [x for x in df.columns if x not in meta_cols]
     confidence_cols = [x for x in metric_cols if x in confidence_cols]
 
