e3863338e7d85f2b7120aeee231b86019eac0774
zivwhd <zivwhd@gmail.com>
2025-06-24 05:56:52 +0300

    next

--- src/funnybirds/evaluate_explainability_par.py
+++ src/funnybirds/evaluate_explainability_par.py
@@ -23,7 +23,7 @@ parser.add_argument('--model', required=True,
                     help='model architecture')
 parser.add_argument('--explainer', required=True,
                     choices=['IntegratedGradients', 'InputXGradient', 'Rollout', 'CheferLRP', 'CustomExplainer',
-                             'xGC', 'xLSC', 'xLC', 'aLSC', 'xRISE', 'xEP', 'xAC', 'xGC++', 'xFG', 'xIG', 'xGIG','xDIX','xMP',
+                             'xGC', 'xLSC', 'xLC', 'aLSC', 'amLSC','xRISE', 'xEP', 'xAC', 'xGC++', 'xFG', 'xIG', 'xGIG','xDIX','xMP',
                              'xDIXv'],
                     help='explainer')
 parser.add_argument('--checkpoint_name', type=str, required=False, default=None,
@@ -147,6 +147,14 @@ def main():
         salc = AutoCompExpCreator(
             desc="AutoComp", segsize=[32], nmasks=[1000], c_opt="Adam", lr=0.1, lr_step=9, lr_step_decay=0.9,  
             epochs=101, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, 
+            c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  c_activation="",
+        )
+        explainer = STEAttributionExplainer(salc, me)
+    elif args.explainer == "amLSC":
+        from lcpe import AutoCompExpCreator
+        salc = AutoCompExpCreator(
+            desc="AutoCompMon", segsize=[32], nmasks=[1000], c_opt="Adam", lr=0.1, lr_step=9, lr_step_decay=0.9,  
+            epochs=101, select_from=10, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, 
             c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  c_activation="",
         )
         explainer = STEAttributionExplainer(salc, me)
