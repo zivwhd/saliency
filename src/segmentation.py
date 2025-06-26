518ddfd0402058c74549ab6c292c779ba6fafe7e
zivwhd <zivwhd@gmail.com>
2025-06-26 07:24:13 +0300

    next

--- src/segmentation.py
+++ src/segmentation.py
@@ -194,7 +194,8 @@ def get_creators_cnn():
         #DixCnnSaliencyCreator(),
         #IGSaliencyCreator(),                                 
         #LTXSaliencyCreator(),
-        IEMPertSaliencyCreator(),        
+        #IEMPertSaliencyCreator(),
+        #ExtPertSaliencyCreator(),        
         #RiseSaliencyCreator(),
     ]
 
@@ -203,7 +204,15 @@ def get_creators_cnn():
             desc="AutoComp", segsize=[32], nmasks=[1000], c_opt="Adam", lr=0.1, lr_step=9, lr_step_decay=0.9,  
             epochs=101, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, 
             c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  c_activation="",
-        )
+        ),
+        CamSaliencyCreator(),
+        #DixCnnSaliencyCreator(),
+        IGSaliencyCreator(),                                 
+        LTXSaliencyCreator(),
+        IEMPertSaliencyCreator(),        
+        ExtPertSaliencyCreator(),        
+        RiseSaliencyCreator(),
+
     ]
 
     return CombSaliencyCreator(runs)
