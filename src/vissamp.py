043df50d728f086bb9e4cb5126cab83edebb56f7
zivwhd <zivwhd@gmail.com>
2025-06-27 23:53:59 +0300

    next

--- src/vissamp.py
+++ src/vissamp.py
@@ -41,6 +41,7 @@ for imgidx, image_info in enumerate(all_images):
     targetidx = image_info.target
     ## {"0": ["n01440764", "tench"]
     target_name = TARGET_NAMES[str(targetidx)][2]
+    logging.info(f"target: {targetidx} {target_name}")
     img=Image.open(image_path)
     img=img.resize((224,224))  
 
