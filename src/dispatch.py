5530589c87ec7b03d48cff72ae8b02fbf6624c60
zivwhd <zivwhd@gmail.com>
2025-06-28 07:13:55 +0300

    next

--- src/dispatch.py
+++ src/dispatch.py
@@ -96,7 +96,7 @@ def get_altcomp_sal_creator():
         desc="AutoCompPos", segsize=[32], nmasks=[1000], c_opt="Adam", lr=0.1, lr_step=9, lr_step_decay=0.9,  
         epochs=101, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0, c_positive=1, 
         c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  c_activation="", cap_response=True
-    ),
+    )
 
 def get_autocomp_sal_creator():
     # desc="AutoCompA",  NOT GOOD segsize=32, nmasks=1000, c_opt="Adam", lr=0.1, lr_step=9, lr_step_decay=0.9, epochs=501, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  
