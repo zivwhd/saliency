33b9b989d409c205784a859d1b726c3aa4ed289f
zivwhd <zivwhd@gmail.com>
2025-07-06 00:05:09 +0300

    next

--- src/dispatch.py
+++ src/dispatch.py
@@ -540,15 +540,17 @@ def get_mwcomp_sal_creator():
     
 def get_obench_sal_creator():
     return CombSaliencyCreator([
-        LTXSaliencyCreator(),
-        IEMPertSaliencyCreator(),
-        RiseSaliencyCreator(),
         AutoCompExpCreator(
             desc="AutoCompSMix32x56", segsize=[32,56], nmasks=[500,500], c_opt="Adam", lr=0.1, lr_step=45, lr_step_decay=0.9,  
             epochs=501, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, 
             c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  c_activation="",
         ),
         DimplVitSaliencyCreator(),
+        LTXSaliencyCreator(),        
+        ExtPertSaliencyCreator(),
+        RiseSaliencyCreator(),
+        IEMPertSaliencyCreator(),
+
     ])
 
 def get_mbench_sal_creator():
