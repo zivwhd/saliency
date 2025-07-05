caf05f865ccf54206e009639ca222e851a464123
zivwhd <zivwhd@gmail.com>
2025-07-05 13:54:48 +0300

    next

--- src/segmentation.py
+++ src/segmentation.py
@@ -197,7 +197,7 @@ def get_creators_cnn():
         #LTXSaliencyCreator(),
         #IEMPertSaliencyCreator(),
         #ExtPertSaliencyCreator(),        
-        #RiseSaliencyCreator(),
+        #RiseSaliencyCreator(),        
     ]
 
     runs = [
@@ -242,6 +242,10 @@ def get_creators_cnn():
                  c_opt="Adam", lr=0.1, lr_step=45, lr_step_decay=0.9, epochs=501, select_from=None,
                 c_mask_completeness=1.0, c_magnitude=0.01, c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False, c_activation="")
 
+    runs = [
+        DixCnnSaliencyCreator()
+    ]
+    
     runs_multi = [
         MultiCompExpCreator(
             desc="MULTSEG",
