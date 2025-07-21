20ffad02f0ab260f1077be84ad48f5b552ddecf3
zivwhd <zivwhd@gmail.com>
2025-07-22 00:03:03 +0300

    next

--- src/funnybirds/evaluate_explainability_par.py
+++ src/funnybirds/evaluate_explainability_par.py
@@ -168,11 +168,11 @@ def main():
         )            
         explainer = STEAttributionExplainer(salc, me)
 
-    elif args.explainer == "mixSLOCfp":         
+    elif args.explainer == "mixSLOCxP":         
         from lcpe import MProbCompExpCreator
         salc = MProbCompExpCreator(
-            desc="SLOCProb", segsize=[32,56], nmasks=[500,500], c_opt="Adam", lr=0.1, lr_step=9, lr_step_decay=0.9,  
-            epochs=101, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, 
+            desc="SLOCProb", segsize=[32,56], nmasks=[500,500], c_opt="Adam", lr=0.1, lr_step=45, lr_step_decay=0.9,  
+            epochs=501, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, 
             c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  c_activation="",
         )
         explainer = STEAttributionExplainer(salc, me)
