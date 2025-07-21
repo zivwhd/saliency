9eb5a5539d022ef0726b85fa6ac07fd0d0ec737e
zivwhd <zivwhd@gmail.com>
2025-07-21 21:27:49 +0300

    next

--- src/dispatch.py
+++ src/dispatch.py
@@ -590,7 +590,7 @@ def get_obench_sal_creator():
         AutoCompExpCreator(
             desc="AutoCompSMix32x56", segsize=[32,56], nmasks=[500,500], c_opt="Adam", lr=0.1, lr_step=45, lr_step_decay=0.9,  
             epochs=501, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, 
-            c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  c_activation="", batch_size=500,
+            c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  c_activation="", batch_size=128,
         ),
         DimplVitSaliencyCreator(),
         LTXSaliencyCreator(),        
