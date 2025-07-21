8644ba6a1384f57317a5db973b61c4f19c0501ca
zivwhd <zivwhd@gmail.com>
2025-07-21 23:58:55 +0300

    next

--- src/dispatch.py
+++ src/dispatch.py
@@ -153,6 +153,14 @@ def get_autoncomp_sal_creator():
 
     ])
 
+def get_autoncomp_sal_creator():
+    return MProbCompExpCreator(
+        desc="SLOCxxProb", segsize=[32,56], nmasks=[500,500], c_opt="Adam", lr=0.1, lr_step=45, lr_step_decay=0.9,  
+        epochs=501, select_from=None, select_freq=3, select_del=1.0, c_mask_completeness=1.0, c_magnitude=0.01, c_positive=0, 
+        c_completeness=0, c_tv=0.1, c_model=0.0, c_norm=False,  c_activation="",
+        ),
+
+
                 
 
 
