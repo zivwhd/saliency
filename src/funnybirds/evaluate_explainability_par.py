ed8cbc2880098d08358237b2578856be784647f3
zivwhd <zivwhd@gmail.com>
2025-10-13 15:14:31 +0300

    msm

--- src/funnybirds/evaluate_explainability_par.py
+++ src/funnybirds/evaluate_explainability_par.py
@@ -24,7 +24,7 @@ parser.add_argument('--model', required=True,
 parser.add_argument('--explainer', required=True,
                     choices=['IntegratedGradients', 'InputXGradient', 'Rollout', 'CheferLRP', 'CustomExplainer',
                              'xGC', 'xLSC', 'xLC', 'aLSC', 'amLSC','mixLSC','mixSLOCxP','SlocSeg','xRISE', 'xEP', 'xAC', 'xGC++', 'xFG', 'xIG', 'xGIG','xDIX','xMP',
-                             'xDIXv', 'xMSMl'],
+                             'xDIXv', 'xMSMs'],
                     help='explainer')
 parser.add_argument('--checkpoint_name', type=str, required=False, default=None,
                     help='checkpoint name (including dir)')
@@ -187,9 +187,9 @@ def main():
         )   
         explainer = STEAttributionExplainer(salc, me)
 
-    elif args.explainer == "xMSMl":
+    elif args.explainer == "xMSMs":
         from msm import MsmExpCreator
-        salc = MsmExpCreator(nsegs=40, alphas=[0], blur_radius=[0])
+        salc = MsmExpCreator(nsegs=200, nmasks=2000, alphas=[0], blur_radius=[0])
         explainer = STEAttributionExplainer(salc, me)        
 
 
